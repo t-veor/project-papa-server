66e6952047c6242d698f9bac565b5b2000d9146e
Tianlin Zhang <t-veor@users.noreply.github.com>
2017-02-07 12:21:12 +0000

    Set up environment for C++ development

--- src/main/java/com/projectpapa/SonicPi/Command.java
+++
@@ -1,9 +0,0 @@
-package com.projectpapa.SonicPi;
-
-/**
- * Simple interface that just indicates the class can be converted into Sonic
- * Pi code.
- */
-public interface Command {
-    String toCode();
-}
