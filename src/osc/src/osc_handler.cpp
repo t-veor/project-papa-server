79510ddffc690c54e2659fc2fb8a0b3fc1d64b9c
Tianlin Zhang <t-veor@users.noreply.github.com>
2017-02-13 16:28:38 +0000

    Changed base handler to deal with only server connectivity messages

--- src/osc/src/osc_handler.cpp
+++ src/osc/src/osc_handler.cpp
@@ -16,30 +16,11 @@ void OSCHandler::handleMessage(std::vector<char> buffer) {
 }
 
 void OSCHandler::handle(oscpkt::Message* msg) {
-    // TODO: fill out bodies
-    if (msg->match("/log/multi_message")) {
-
-    }
-    else if (msg->match("/log/info")) {
-
-    }
-    else if (msg->match("/error")) {
-
-    }
-    else if (msg->match("/syntax_error")) {
-
-    }
-    else if (msg->match("/update-info-text")) {
-
-    }
-    else if (msg->match("/exited")) {
+    if (msg->match("/exited")) {
         signal_server_stop = true;
     }
     else if (msg->match("/exited-with-boot-error")) {
         signal_server_stop = true;
-    }
-    else if (msg->match("/runs/all-completed")) {
-        
     }
     else if (msg->match("/ack")) {
         server_started = true;
